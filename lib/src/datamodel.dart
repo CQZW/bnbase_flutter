ff02725d6c0b1649d0df9bf4d06682434abd1565
zzl <646172505@qq.com>
2021-03-18 11:14:05 +0800

    添加网络封装

---
+++ lib/src/datamodel.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+///接口返回的通用数据结构
+class SResBase<T> {
+  bool get mSuccess => mCode == 0;
+
+  ///服务器返回的错误码,这里占用几个
+  ///1:错误,不知道具体的错误,都是1,这里没有使用,建议这样使用,
+  ///2:服务器返回数据异常
+  ///3:网络请求异常
+  late int mCode;
+
+  ///服务器返回的消息,用于调试吧
+  late String mMsg;
+
+  ///需要UI显示的消息
+  String? mUIMsg;
+
+  ///这里应该使用泛型之类的,,但是不喜欢到处是尖括号,,
+  ///dart居然可以不设置 T,,,那就没有到处尖括号了,good...
+  T? mData;
+
+  ///创建通用对象,添加成功的描述
+  SResBase.infoWithOKString(String okstr, [cdoe = 0, fetchuimsg = false]) {
+    mCode = cdoe;
+    mMsg = okstr;
+    mUIMsg = fetchuimsg ? okstr : null;
+    mData = null;
+  }
+
+  ///创建通用对象,添加失败的描述,错误码默认1
+  SResBase.infoWithErrorString(String errstr, [cdoe = 1, fetchuimsg = false]) {
+    mCode = cdoe;
+    mMsg = errstr;
+    mUIMsg = fetchuimsg ? errstr : null;
+
+    mData = null;
+  }
+  SResBase.baseWithData(Map<String, dynamic> data) {
+    if (data["code"] == null) {
+      SResBase.infoWithErrorString("请求服务器失败,请稍微再试", 2);
+      return;
+    }
+    if (data["code"] is! int) {
+      SResBase.infoWithErrorString("服务器数据,请稍微再试", 2);
+      return;
+    }
+    mCode = data["code"] as int;
+    mMsg = data["msg"] ?? "";
+    mData = data["data"];
+
+    mUIMsg = data["uiMsg"] != null ? data["uiMsg"] as String : null;
+  }
+
+  ///转换成另外一个类型的数据,就拷贝了
+  SResBase<T> toTypeResb<T>() {
+    return SResBase<T>.infoWithOKString(this.mMsg, this.mCode);
+  }
+}
+
+abstract class SAutoEx {
+  SAutoEx([Map<String, dynamic>? json]) {
+    if (json != null) fetchIt(json);
+  }
+
+  ///JSON->对象,自己必须实现
+  void fetchIt(Map<String, dynamic> json);
+
+  ///对象 -> JSON,自己必须实现
+  Map<String, dynamic> toJson();
+
+  ///将自己存储到本地
+  Future<bool> dumpSelf(String key) async {
+    SharedPreferences perfs = await SharedPreferences.getInstance();
+    return perfs.setString(key, json.encode(toJson()));
+  }
+
+  ///加载自己存储的数据
+  static Future<Map<String, dynamic>?> loadSelf(String key) async {
+    var perfs = await SharedPreferences.getInstance();
+    var t = perfs.getString(key);
+    if (t != null) return json.decode(t);
+    return null;
+  }
+
+  static bool _someopinited = false;
+
+  ///数据模型一些必须的初始化动作,
+  ///比如加载当前用户存储到磁盘的数据,由于读取磁盘是异步,所以让外层使用非常不方便
+  ///这里提供一个初始化操作的机会
+  static Future<SResBase> someInitOp() async {
+    if (_someopinited) throw Exception("some op had inited");
+    _someopinited = true;
+    return SResBase.infoWithOKString("成功");
+  }
+}
---
+++ lib/src/netwapper.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+import 'datamodel.dart';
+/*
+网络请求封装
+
+请求就4个参数,token作为参数传递,没有用HTTP自带的认证方式
+req => { token,lang,version,client,data },
+
+返回就3个数据,
+resb => { code,data,msg}
+
+*/
+
+abstract class NetWapper {
+  late final Dio dio;
+
+  ///子类自己实现,因为需要自己实现  getToken,getLang等
+  //static NetWapper _g_nstance;
+//   static NetWapper shareClient() {
+//     if (_g_nstance == null) _g_nstance = NetWapper(_g_baseurl);
+//     return _g_nstance;
+//   }
+
+  late final String baseurl;
+  NetWapper(String baseurl,
+      {int connectTimeout = 30 * 1000, int receiveTimeout = 30 * 1000}) {
+    this.baseurl = baseurl;
+    _initNetWapper(BaseOptions(
+        connectTimeout: connectTimeout, receiveTimeout: receiveTimeout));
+  }
+
+  void _initNetWapper(BaseOptions opt) {
+    dio = Dio(opt);
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (client) {
+      client.badCertificateCallback = verificationCert;
+    };
+  }
+
+  //证书校验
+  bool verificationCert(X509Certificate cert, String host, int port) => true;
+
+  ///获取认证token
+  Future<String> getToken(); // => '';
+
+  ///获取设备语言设置
+  Future<String> getLang(); // => 'zh-CN';
+
+  ///获取设备ID
+  Future<String> getDeviceId();
+
+  ///获取版本信息
+  Future<String> getVersion();
+
+  ///请求之前做些额外处理,比如数据加密
+  Future<Map<String, dynamic>> preDeal(
+      String path, Map<String, dynamic> param) async {
+    ///如果需要加密,这里处理,只加密data字段
+    //r["data"] = json.encode(param != null ? param : Map());
+    //r["data"] = param != null ? param : Map();
+
+    return param;
+  }
+
+  ///预处理header,公共参数放到header里面
+  Future<Map<String, dynamic>> preHeader(
+      String path, Map<String, dynamic> param) async {
+    Map<String, dynamic> r = Map<String, dynamic>();
+    r["version"] = await getVersion();
+    r["lang"] = await getLang();
+    r['token'] = await getToken();
+    r['deviceId'] = await getDeviceId();
+    if (defaultTargetPlatform == TargetPlatform.iOS)
+      r['client'] = 'ios';
+    else if (defaultTargetPlatform == TargetPlatform.android)
+      r['client'] = 'android';
+    else
+      r['client'] = 'unkown';
+    return r;
+  }
+
+  ///请求之后做些额外处理,比如数据解密,,
+  Future<Map<String, dynamic>> dealPost(String resbstr) async {
+    Map<String, dynamic> r = json.decode(resbstr);
+
+    ///如果要解密,只处理data字段即可
+    ///r['data'] = decryp( r['data'] );
+    return r;
+  }
+
+  Future<SResBase> postPath(String path, Map<String, dynamic> param) async {
+    try {
+      String url = makeApiPath(path);
+      var reqparam = await preDeal(path, param);
+      var header = await preHeader(path, param);
+      log("req url:" +
+          url +
+          " param:" +
+          reqparam.toString() +
+          " header:" +
+          header.toString());
+      Response<String> resb = await dio.post(url,
+          queryParameters: reqparam, options: Options(headers: header));
+
+      log("resb url:" + url + " data:" + resb.data);
+      return SResBase.baseWithData(await dealPost(resb.data));
+    } catch (e) {
+      log("resb exp:" + e.toString());
+      return SResBase.infoWithErrorString("网络请求异常", 3);
+    }
+  }
+
+  String makeApiPath(String path) {
+    //如果是http开头就是全路径
+    if (path.startsWith("http")) return path;
+    return this.baseurl + path;
+  }
+}
